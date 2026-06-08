f1251f130190f3bc9f49eea639f5816d7c77d239
schumjas <schumjas@oregonstate.edu>
2026-06-07 23:32:33 -0700

    completed the assignment

--- iris_data.py
+++ iris_data.py
@@ -2,16 +2,36 @@ import pandas as pd
 
 
 def parse_line(line):
-    pass
+    elm = []
+    line = line.strip()
+    fields = line.split('|')
+    for i in range(len(fields) - 1):
+        elm.append(float(fields[i]))
+    elm.append(fields[4])
+    return elm
 
 
 def add_to_dict(parsed_line, data_dict):
-    pass
+    if len(parsed_line) != len(data_dict.keys()):
+        raise ValueError("number of fields does not match dictionary keys.")
+    for i in list(data_dict.keys()):
+        data_dict[i] = parsed_line[i]
+
 
 
 def load_data(filename):
-    pass
+    data_dict = {sepal_length:[],
+                 sepal_width:[],
+                 petal_length:[],
+                 petal_width:[],
+                 species:[]}
+    open(filename, 'r')
+    for i in list(a.keys()):
+        parsed = parse_line(a[i])
+        add_to_dict(parsed, data_dict)
+    return pd.DataFrame(data_dict)
 
 
 def species_mean(data, species, measurement):
-    pass
+    data.loc(data["species"] == species)
+    return data[measurement].mean()
